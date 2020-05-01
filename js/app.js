3fe17bfeccb49f43f93b317acb0dbc9880038bf6
Trevor Stubbs <stubbste@gmail.com>
2020-04-30 19:32:50 -0700

    replace breaks with returns

--- js/app.js
+++ js/app.js
@@ -8,12 +8,10 @@ function welcomeUser(){
 }
 
 // Activated by the guessingGame Button
-// Linter isn't happy but this function is being called by the even listener in the HTML.
+// Linter isn't happy but this function is being called by the event listener in the HTML.
 function guessGame(){
   // variable to collect correct answers.
   var userScore = 0;
-  // array to hold correct and incorrect.
-  var correctArray = ['correct', 'incorrect'];
 
   // 1st y/n question
   function firstQuestion(){
@@ -46,71 +44,6 @@ function guessGame(){
     noIsCorrect(questionFive);
   }
 
-  // Variables to set up the number guessing game
-  function numberGuess(){
-    var number = Math.floor(Math.random() * 10) + 1;
-    var userGuess = 0;
-    var correctGuess = false;
-
-    alert('I will give you 4 tries to guess a number between 1 and 10.');
-
-    for(var i = 4; i >= 1; i--){
-      userGuess = Number(prompt('You have ' + i + ' tries left. Please make a guess: '));
-
-      if(userGuess === number){
-        //console.log('correct');
-        alert('That\'s it! I was thinking of ' + number + '.');
-        userScore++;
-        correctGuess = true;
-        break;
-      } else {
-        if(userGuess > number){
-          alert('Your guess was too big.');
-        } else {
-          alert('Your guess was too small.');
-        }
-      }
-    }
-
-    // Make this alert if the user didn't get the answer correct
-    if(correctGuess === false){
-      //console.log('incorrect');
-      alert('The number was ' + number + '.');
-    }
-  }
-
-  //Setup variables for the fill in the blank question
-  function planeQuestion(){
-    var answerArray = ['cessna', 'beechcraft', 'bombardier', 'embraer', 'piper'];
-    var correctGuess = false;
-    alert('I will give you 6 attempts to guess a make of airplane that I have flown');
-
-    //outer for loop to limit the number of guess by the user
-    for(var j = 6; j >= 1; j--){
-      var arrayGuess = prompt('You have ' + j + ' more attempts. What have I flown?').toLowerCase();
-
-      //inner for loop to iterate through the array to check to see if the guess matches an answer
-      for(var n = 0; n < answerArray.length; n++){
-        if(arrayGuess === answerArray[n]){
-          alert('Awesome job, you are correct, I have flown the ' + arrayGuess + ' brand of airplane! All of the correct answers are : ' + answerArray[0] + ', ' + answerArray[1] + ', ' + answerArray[2] + ', ' + answerArray[3] + ', ' + 'and ' + answerArray[4] + '.');
-          userScore++;
-          correctGuess = true;
-          break;
-        }
-      }
-
-      //break out of the outer loop if a guess was correct.
-      if(correctGuess === true){
-        break;
-      }
-    }
-
-    // If the guess was not correct then give the user the answers.
-    if(correctGuess === false){
-      alert('I am sorry you did not guess any correctly. The possible answers were: ' + answerArray[0] + ', ' + answerArray[1] + ', ' + answerArray[2] + ', ' + answerArray[3] + ', ' + 'and ' + answerArray[4] + '.');
-    }
-  }
-
   // function logic for yes
   function yesIsCorrect(answer){
     if(answer === 'yes' || answer === 'y'){
@@ -135,6 +68,9 @@ function guessGame(){
     }
   }
 
+  // array to hold correct and incorrect.
+  var correctArray = ['correct', 'incorrect'];
+
   // function to call correct
   function correct(){
     return correctArray[0];
@@ -145,16 +81,63 @@ function guessGame(){
     return correctArray[1];
   }
 
-  // calling the questions.
-  firstQuestion();
-  secondQuestion();
-  thirdQuestion();
-  fourthQuestion();
-  fifthQuestion();
-  // Calling the number guessing game.
-  numberGuess();
-  // calling fill in the blank question.
-  planeQuestion();
+  // Variables to set up the number guessing game
+  function numberGuess(){
+    var number = Math.floor(Math.random() * 10) + 1;
+    var userGuess = 0;
+
+    alert('I will give you 4 tries to guess a number between 1 and 10.');
+
+    for(var i = 4; i >= 1; i--){
+      userGuess = Number(prompt('You have ' + i + ' tries left. Please make a guess: '));
+
+      if(userGuess === number){
+        alert('That\'s it! I was thinking of ' + number + '.');
+        userScore++;
+        return;
+      } else {
+        if(userGuess > number){
+          alert('Your guess was too big.');
+        } else {
+          alert('Your guess was too small.');
+        }
+      }
+    }
+
+    // Make this alert if the user didn't get the answer correct
+    alert('The number was ' + number + '.');
+  }
+
+  //Setup variables for the fill in the blank question
+  function planeQuestion(){
+    var answerArray = ['cessna', 'beechcraft', 'bombardier', 'embraer', 'piper'];
+    alert('I will give you 6 attempts to guess a make of airplane that I have flown');
+
+    //outer for loop to limit the number of guess by the user
+    for(var j = 6; j >= 1; j--){
+      var arrayGuess = prompt('You have ' + j + ' more attempts. What have I flown?').toLowerCase();
+
+      //inner for loop to iterate through the array to check to see if the guess matches an answer
+      for(var n = 0; n < answerArray.length; n++){
+        if(arrayGuess === answerArray[n]){
+          alert('Awesome job, you are correct, I have flown the ' + arrayGuess + ' brand of airplane! All of the correct answers are : ' + answerArray[0] + ', ' + answerArray[1] + ', ' + answerArray[2] + ', ' + answerArray[3] + ', ' + 'and ' + answerArray[4] + '.');
+          userScore++;
+          //Return out of the function
+          return;
+        }
+      }
+    }
+
+    // If the guess was not correct then give the user the answers.
+    alert('I am sorry you did not guess any correctly. The possible answers were: ' + answerArray[0] + ', ' + answerArray[1] + ', ' + answerArray[2] + ', ' + answerArray[3] + ', ' + 'and ' + answerArray[4] + '.');
+  }
+
+  // Array that holds question functions
+  var allYesOrNoQuestions = [firstQuestion(), secondQuestion(), thirdQuestion(), fourthQuestion(), fifthQuestion(), numberGuess(), planeQuestion()];
+  // calling the questions with a for loop
+  for(var i = 0; i < allYesOrNoQuestions.length; i++){
+    allYesOrNoQuestions[i];
+  }
 
   //tell the user their score.
   alert('You answered ' + userScore + ' questions correctly. Thank you for playing!');
@@ -162,4 +145,3 @@ function guessGame(){
 
 // calling welcome function
 welcomeUser();
-
